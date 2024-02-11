bfed72daaa97256095c2a7b62c2d8535aa4a8505
Sergey <codeswiths@gmail.com>
2024-02-11 18:46:03 +0600

    The program works correctly. Task solved.

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -5,7 +5,7 @@ public class Main {
         PhoneBook phoneBook = new PhoneBook();
         phoneBook.addContacts("Sergey", "123456789");
         phoneBook.addContacts("Sergey", "987654321");
-        phoneBook.addContacts("Ivan", "987321654");
+        phoneBook.addContacts("Sergey", "987321654");
         phoneBook.addContacts("Victoria", "456789123");
         phoneBook.addContacts("Ivan", "654231897");
         phoneBook.addContacts("Victoria", "789123456");
